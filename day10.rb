da7156a3edfff7309edabd6be153accbe2cb4253
DhanyaJoseK <dhanya.jose2@gmail.com>
2016-06-15 15:05:13 +0530

    Add files via upload

---
+++ day10.rb
@@ -0,0 +1,9 @@
+input = '1113222113'
+
+
+40.times  { input = input.gsub(/(.)\1*/) {|i| i.size.to_s + i[0] } } #.gsub(/matched_sym/) {|sym| "-#{sym}-"}
+
+puts input.length
+
+
+#sub size of digit and digit itself in place of regular expression
